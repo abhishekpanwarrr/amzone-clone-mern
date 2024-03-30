724a165c0fe89754211f6d130cccaf7e3eca4d7e
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2024-03-30 16:15:51 +0530

    model timestampls

--- backend/models/Payment.js
+++ backend/models/Payment.js
@@ -1,31 +1,36 @@
 import mongoose from "mongoose";
 
-const paymentSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
+const paymentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    orderItems: {
+      type: Array,
+      required: true,
+    },
+    razorpayOrderId: {
+      type: String,
+      required: true,
+    },
+    razorpayPaymentId: {
+      type: String,
+      required: true,
+    },
+    razorpaySignature: {
+      type: String,
+      required: true,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
   },
-  orderItems: {
-    type: Array,
-    required: true,
-  },
-  razorpayOrderId: {
-    type: String,
-    required: true,
-  },
-  razorpayPaymentId: {
-    type: String,
-    required: true,
-  },
-  razorpaySignature: {
-    type: String,
-    required: true,
-  },
-  totalAmount: {
-    type: Number,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Payment = mongoose.model("Payment", paymentSchema);
 export default Payment;
